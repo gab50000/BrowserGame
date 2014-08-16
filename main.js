10020105acb730c23729d6c97758ae3522e60cc4
gk <gabriel.kabbe@mail.de>
2014-08-16 02:41:17 +0200

    show sprites in canvas

--- main.js
+++ main.js
@@ -2,5 +2,10 @@ function draw(){
 	var canvas = document.getElementById('tutorial');
 	if (canvas.getContext){
 	  var ctx = canvas.getContext('2d');
+	  var img = new Image();
+	  img.onload = function(){
+	  	ctx.drawImage(img, 0, 0, 800, 600);
+	  };
+	  img.src = "images/zeldaoracleofages_link_sheet.png";
 	}
 }
