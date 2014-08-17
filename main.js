eacccce16653b953ad76ae745c2df93cddb59e6c
gk <gabriel.kabbe@mail.de>
2014-08-17 23:06:02 +0200

    this problems solved

--- main.js
+++ main.js
@@ -8,7 +8,9 @@ function draw(){
 	  var posy = 200;
 
 	  img.onload = function(){
-	  	game = new Game(img, ctx);
+	  	var game = new Game(img, ctx);
+	  	game.draw();
+	  	game.update();
 	  	game.run();
 		  // var sprite_arr = create_sprite_array(img, 32, 32);
 		  // sprite_arr[22].draw(ctx, 200, 200);
@@ -73,23 +75,28 @@ Sprite.prototype.draw = function(ctx, dest_posx, dest_posy){
 
 function Game(img, ctx){
 	this.fps = 60;
-	this.i = 10;
+	this.i = 0;
 	this.sprite_arr = create_sprite_array(img, 32, 32);
 	this.ctx = ctx;
 }
 
 Game.prototype.run = function(){
+	var that = this;
+	console.log("Ich bin"+that);
 	this.update();
 	this.draw();
-	window.requestAnimationFrame(this.run);
+	window.requestAnimationFrame(this.run.bind(this));
+	//bind the this pointer to the function run
 	//requestAnimationFrame needs the function as argument, not the result of the function!!
 };
 
 Game.prototype.update = function(){
+	console.log(this.i);
 	this.i += 1;
 };
 
 Game.prototype.draw = function(){
+	this.ctx.
 	this.sprite_arr[this.i%this.sprite_arr.length].draw(this.ctx, 200,200);
 
 };
