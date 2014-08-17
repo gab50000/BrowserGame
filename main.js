5f48029c6fd41ab157256fce7ef6658770ccbed1
gk <gabriel.kabbe@mail.de>
2014-08-17 23:47:57 +0200

    sprites update, canvas refreshes

--- main.js
+++ main.js
@@ -62,11 +62,11 @@ Link.prototype.moveRight = function() {
 //constructor takes coordinate of upper left
 function Sprite(img, xpos, ypos, width, height){
 	this.img = img;
-	this.xpos = xpos;
-	this.ypos = ypos;
+	this.xpos = xpos; //xpos in the original sprite pic
+	this.ypos = ypos; //ypos in ....
 	this.width = width;
 	this.height = height;
-	this.scaling = 3;
+	this.scaling = 2;
 }
 
 Sprite.prototype.draw = function(ctx, dest_posx, dest_posy){
@@ -74,29 +74,36 @@ Sprite.prototype.draw = function(ctx, dest_posx, dest_posy){
 };
 
 function Game(img, ctx){
-	this.fps = 60;
+	this.fps = 5;
 	this.i = 0;
 	this.sprite_arr = create_sprite_array(img, 32, 32);
 	this.ctx = ctx;
 }
 
 Game.prototype.run = function(){
-	var that = this;
-	console.log("Ich bin"+that);
+	// console.log("Ich bin"+this);
 	this.update();
 	this.draw();
-	window.requestAnimationFrame(this.run.bind(this));
+	var that = this;
+	setTimeout(function(){
+		window.requestAnimationFrame(that.run.bind(that));
+	}, 1000/that.fps); 
 	//bind the this pointer to the function run
 	//requestAnimationFrame needs the function as argument, not the result of the function!!
 };
 
 Game.prototype.update = function(){
-	console.log(this.i);
+	// console.log(this.i);
 	this.i += 1;
 };
 
 Game.prototype.draw = function(){
-	this.ctx.
-	this.sprite_arr[this.i%this.sprite_arr.length].draw(this.ctx, 200,200);
+	var sa = this.sprite_arr;
+	if (this.i == this.sprite_arr.length){
+		this.i = 0;
+	}
+	// console.log(sa[this.i].width);
+	this.ctx.clearRect(200, 200, sa[this.i].width*sa[this.i].scaling, sa[this.i].height*sa[this.i].scaling);
+	this.sprite_arr[this.i].draw(this.ctx, 200,200);
 
 };
