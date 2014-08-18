450f492b1de7fc0812e1a5bac7e1726cb5f425ae
gk <gabriel.kabbe@mail.de>
2014-08-18 23:42:07 +0200

    key events are registered. canvas update wrong

--- main.js
+++ main.js
@@ -1,16 +1,20 @@
 function draw(){
 	var canvas = document.getElementById('tutorial');
 	if (canvas.getContext){
+	  canvas.style.background = "purple";
 	  var ctx = canvas.getContext('2d');
 	  var img = new Image();
 	  img.src = "images/la_link.png";
 	  var posx = 200;
 	  var posy = 200;
 
+
 	  img.onload = function(){
 	  	var game = new Game(img, ctx);
-	  	game.draw();
-	  	game.update();
+   	    window.addEventListener('keydown',game.check_push.bind(game),false);
+	    window.addEventListener('keyup',game.check_release.bind(game),false);
+	  	// game.draw();
+	  	// game.update();
 	  	game.run();
 		  // var sprite_arr = create_sprite_array(img, 32, 32);
 		  // sprite_arr[22].draw(ctx, 200, 200);
@@ -33,30 +37,35 @@ function create_sprite_array(img, sprite_width, sprite_height){
 
 }
 
-function Link(img){
+function Link(sprite_arr){
+	this.states = ["shield", "defending", "jumping", "swimming", "walking", "standing"]
+	self.facing = "down";
 	this.x = 0;
 	this.y = 0;
-	this.img = img;
+	this.sprite_arr = sprite_arr;
+	self.walking = false;
+	self.shield = false;
 }
 
 Link.prototype.draw = function(ctx) {
-  draw_sprite(ctx, this.img, this.x, this.y, 16);
+  // draw_sprite(ctx, this.img, this.x, this.y, 16);
+  this.sprite_arr[0].draw(ctx, this.x, this.y);
 };
 
-Link.prototype.moveLeft = function() {
-  this.x -= 1;
+Link.prototype.move_left = function() {
+  this.x -= 10;
 };
 
-Link.prototype.moveRight = function() {
-  this.x += 1;
+Link.prototype.move_right = function() {
+  this.x += 10;
 };
 
-Link.prototype.moveUp = function() {
-  this.y -= 1;
+Link.prototype.move_up = function() {
+  this.y -= 10;
 };
 
-Link.prototype.moveRight = function() {
-  this.y += 1;
+Link.prototype.move_down = function() {
+  this.y += 10;
 };
 
 //constructor takes coordinate of upper left
@@ -76,9 +85,34 @@ Sprite.prototype.draw = function(ctx, dest_posx, dest_posy){
 function Game(img, ctx){
 	this.fps = 5;
 	this.i = 0;
-	this.sprite_arr = create_sprite_array(img, 32, 32);
+	sprite_arr = create_sprite_array(img, 32, 32);
 	this.ctx = ctx;
+	this.buttons_pressed = {"up":false, "down":false, "right":false, "left":false};
+	this.link = new Link(sprite_arr);
 }
+
+Game.prototype.check_push = function(e){
+	// console.log("just checking push");
+	var code = e.keyCode;
+	switch (code){
+		case 37 : this.buttons_pressed["left"] = true; break;
+		case 38 : this.buttons_pressed["up"] = true; break;
+		case 39 : this.buttons_pressed["right"] = true; break;
+		case 40 : this.buttons_pressed["down"] = true; break;
+
+	}
+};
+Game.prototype.check_release = function(e){
+	// console.log("just checking release");
+	var code = e.keyCode;
+	switch (code){
+		case 37 : this.buttons_pressed["left"]  = false; break;
+		case 38 : this.buttons_pressed["up"]    = false; break;
+		case 39 : this.buttons_pressed["right"] = false; break;
+		case 40 : this.buttons_pressed["down"]  = false; break;
+
+	}
+};
 
 Game.prototype.run = function(){
 	// console.log("Ich bin"+this);
@@ -93,17 +127,43 @@ Game.prototype.run = function(){
 };
 
 Game.prototype.update = function(){
-	// console.log(this.i);
-	this.i += 1;
+	console.log(this.buttons_pressed);
+
+	if (this.buttons_pressed["left"] == true){
+		this.link.move_left();
+	}
+	if (this.buttons_pressed["right"] == true){
+		this.link.move_right();
+	}
+	if (this.buttons_pressed["up"] == true){
+		this.link.move_up();
+	}
+	if (this.buttons_pressed["down"] == true){
+		this.link.move_down();
+	}
+
 };
 
 Game.prototype.draw = function(){
 	var sa = this.sprite_arr;
-	if (this.i == this.sprite_arr.length){
-		this.i = 0;
-	}
+	// if (this.i == this.sprite_arr.length){
+	// 	this.i = 0;
+	// }
 	// console.log(sa[this.i].width);
-	this.ctx.clearRect(200, 200, sa[this.i].width*sa[this.i].scaling, sa[this.i].height*sa[this.i].scaling);
-	this.sprite_arr[this.i].draw(this.ctx, 200,200);
+	this.ctx.clearRect(this.link.x, this.link.y, this.link.sprite_arr[0].width*this.link.sprite_arr[0].scaling, this.link.sprite_arr[0].height*this.link.sprite_arr[0].scaling);
+	this.link.draw(this.ctx);
 
 };
+
+//Event Listener
+
+function check(e){
+	var code = e.keyCode;
+	switch (code){
+		case 37 : console.log("left"); break;
+		case 38 : console.log("up"); break;
+		case 39 : console.log("right"); break;
+		case 40 : console.log("down"); break;
+	}
+
+}
